4f6a45260e0ee404e8e775402630e419c9f47535
Matheus <matheusbenatti2@gmail.com>
2023-03-15 10:43:24 -0300

    Programa finalizado

--- vetor.js
+++ vetor.js
@@ -1,8 +1,36 @@
 let cidade = []
+let res = document.getElementById('res')
+let valor_cidade = []
 
 function inserir(){
 
-    let valor_cidade = document.getElementById('cidade')
-    cidade.push('Itapema')
-    console.log(cidade)
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.push(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+
+
+
+}
+
+function excluir(){
+
+    valor_cidade.pop()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+function excluir_final(){
+    valor_cidade.shift()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+
+
 }
